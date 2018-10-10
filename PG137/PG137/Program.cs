9651778668df64603156bdee46a0a6998de62811
Vivien Renee <vivien.renee.b@gmail.com>
2018-10-09 20:52:57 -0700

    Add files via upload

---
+++ PG137/PG137/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number firstNumber = new Number();
+            firstNumber.Amount = 7.2m;
+            Console.WriteLine(firstNumber.Amount);
+            Console.ReadLine();
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+
+    }   
+}
