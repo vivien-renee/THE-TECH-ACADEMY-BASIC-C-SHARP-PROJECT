caf93acc0156f4ff7dcb59f8bed48c8443ab0d02
Vivien Renee <vivien.renee.b@gmail.com>
2018-10-09 19:15:43 -0700

    Add files via upload

---
+++ PG134/PG134/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+                var currentDay = Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+           
+                
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
