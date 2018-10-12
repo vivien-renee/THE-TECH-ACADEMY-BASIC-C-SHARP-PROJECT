eaaa2bb49caad67ccb0567bff3cdf371b7d8571c
Vivien Renee <vivien.renee.b@gmail.com>
2018-10-12 13:04:27 -0700

    Add files via upload

---
+++ PG156/PG156/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PG156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Pick a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            DateTime dt = DateTime.Now;
+            dt = dt.AddHours(number);
+            Console.WriteLine("In " + number + " Hour/s, it will be "+ dt);
+            Console.ReadLine();
+        }
+    }
+}
