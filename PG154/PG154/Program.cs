0f2b0f4ac8ac347159e4819d776bc3c15bab82e0
Vivien Renee <vivien.renee.b@gmail.com>
2018-10-12 12:20:21 -0700

    Add files via upload

---
+++ PG154/PG154/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PG154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number.");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\christopher benben\Desktop\C#\C.txt", text);
+            System.Console.WriteLine("{0} has been logged.", text);
+            Console.ReadLine();
+
+        }
+    }
+}
