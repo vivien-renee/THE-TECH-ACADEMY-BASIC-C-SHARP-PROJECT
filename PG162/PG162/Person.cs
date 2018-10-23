5b64328f86bc7b6e50faa4eb10cfc0b4d2365f27
Vivien Renee <vivien.renee.b@gmail.com>
2018-10-22 18:33:29 -0700

    Add files via upload

---
+++ PG162/PG162/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG162
+{
+  
+
+    public  class Person
+    {
+
+        public Person(string FirstName) : this(FirstName, "Unknown")
+        {
+        }
+
+        public Person(string FirstName, string LastName)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+
+        }
+
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public  void SayName()
+        { Console.WriteLine("Name: " + firstName + " " + lastName); }
+    }
+
+
+    
+    
+
+}
+
---
+++ PG162/PG162/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const string BossName = "Vivien";
+            Console.WriteLine("The boss today is {0}", BossName);
+            var person = new Person("Bob", /*"Bobber"*/);
+            person.SayName();
+
+            
+
+            Console.ReadLine();
+         
+        }
+    }
+}
