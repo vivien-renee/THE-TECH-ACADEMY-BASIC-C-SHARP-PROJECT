f9e61265bd45df52340ed90f892cb277b3de3dae
Vivien Renee <vivien.renee.b@gmail.com>
2018-10-22 22:13:32 -0700

    Add files via upload

---
+++ PG165/PG165/Age.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG165
+{
+    public class Age
+    {
+
+        public void findYear ()
+
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (age < 1)
+            {
+                throw new Exception();
+            }
+
+            
+
+
+
+            var yearToday = DateTime.Now.Year;
+
+            int yearBorn = yearToday - age;
+
+            Console.WriteLine("You were born in " + yearBorn);
+
+        }
+
+
+
+
+    }
+}
---
+++ PG165/PG165/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Age age = new Age();
+
+
+            try
+            {
+                age.findYear();
+            }
+
+
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter a positive number.");
+                return;
+
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a whole number other than zero.");
+                return;
+
+            }
+
+           
+
+
+
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            
+
+
+
+
+        }
+    }
+}
