8582f966404bc450735d4671a5cbfbbcf5bfd320
Vivien Renee <vivien.renee.b@gmail.com>
2018-10-22 21:34:27 -0700

    Add files via upload

---
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class TwentyOneGame : Game
+    {
+        public override void Play () //the override means we will define what Play() means to this game
+        {
+            throw new NotImplementedException();
+        }
+        public override void ListPlayers ()
+        {
+            Console.WriteLine("21 Players");
+            base.ListPlayers();
+        }
+    }
+}
